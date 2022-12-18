d4052a22a0da8105cd3d59c5a3c909471afef70e
F.K <19738301+Entepotenz@users.noreply.github.com>
2022-12-18 17:49:17 +0100

    sh: remove BACKUP_FOLDER always

--- helper_download_data_and_create_backup.sh
+++ helper_download_data_and_create_backup.sh
@@ -7,8 +7,7 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
 readonly SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 readonly BACKUPS_FOLDER="$SCRIPT_DIR/scripts/backups"
+trap 'rm -rf -- "$BACKUPS_FOLDER"' EXIT SIGTERM SIGINT
 
 "$SCRIPT_DIR/scripts/collect_data_from_imap.sh"
 "$SCRIPT_DIR/scripts/backup_data_using_duplicati.sh"
-
-rm -rf "$BACKUPS_FOLDER"
