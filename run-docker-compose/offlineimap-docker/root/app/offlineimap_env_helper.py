bc484fae1e539829c0c9217886bc9a9b4fdd9047
F.K. <19738301+Entepotenz@users.noreply.github.com>
2025-02-02 17:14:20 +0100

    flake8: reformat python file

--- run-docker-compose/offlineimap-docker/root/app/offlineimap_env_helper.py
+++ run-docker-compose/offlineimap-docker/root/app/offlineimap_env_helper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
 
+
 def get_env_variable(env_variable_name: str):
-    return os.getenv(env_variable_name, default='')
+    return os.getenv(env_variable_name, default="")
